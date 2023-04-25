afc930a39710d5be563e5d32c6968bddef4179a6
unknown <wesgaspar1@gmail.com>
2023-04-25 15:54:46 -0300

    Exercicios

---
+++ POO/Testa_Exer2.java
@@ -0,0 +1,13 @@
+package POO;
+
+public class Testa_Exer2 {
+
+	public static void main(String[] args) {
+		
+		Classe_Exer2 funcionario1 = new Classe_Exer2 ("Jose","443.496.580-84",45,2500,"dev jr");
+		Classe_Exer2 funcionario2 = new Classe_Exer2 ("Larissa","346.927.950-00",22,4600,"dev pleno");
+		funcionario1.imprimir();
+		funcionario2.imprimir();
+	}
+
+}
